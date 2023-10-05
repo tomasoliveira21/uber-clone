55fe4f97de009033d5b6c5d71eaa52eb46a9fa2d
tomasoliveira21 <tomasoliveira1018@gmail.com>
2023-10-05 16:32:00 +0100

    Setting up and implementing Redux

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Let's build Uber</Text>
       </View>
