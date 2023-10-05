c97d7e6102dd0c2388c21c15b3d2607dd56af394
tomasoliveira21 <tomasoliveira1018@gmail.com>
2023-10-05 15:48:25 +0100

    benfica sempre

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ser Benfiquista{'\n'}
+      É ter na alma a chama imensa{'\n'}
+      Que nos conquista{'\n'}
+      E leva à palma a luz intensa</Text>
       <StatusBar style="auto" />
     </View>
   );
