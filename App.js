933912cba0c3329b7a28d74d8b8d01253b3fcdda
tomasoliveira21 <tomasoliveira1018@gmail.com>
2023-10-05 17:13:04 +0100

    Home Component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
